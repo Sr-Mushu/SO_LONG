0ad98899cda95b05c7239c4cfe0f4787a26ba40d
David Gabriel silva da costa <dagabrie@c2r8s7.42lisboa.com>
2023-07-27 17:59:29 +0100

    jul-27

--- files/so_long.c
+++ files/so_long.c
@@ -6,15 +6,41 @@
 /*   By: dagabrie <dagabrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 16:08:21 by dagabrie          #+#    #+#             */
-/*   Updated: 2023/07/27 16:13:20 by dagabrie         ###   ########.fr       */
+/*   Updated: 2023/07/27 17:40:51 by dagabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int main(int argn, char **args)
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+int	main(void)
 {
-	
-	
-	exit(1);
+
+#if 0
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 500, 500, "SO_LONG");
+	mlx_loop(mlx);
+#else
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+#endif
 }
--- files/so_long.h
+++ files/so_long.h
@@ -6,14 +6,14 @@
 /*   By: dagabrie <dagabrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 16:08:24 by dagabrie          #+#    #+#             */
-/*   Updated: 2023/07/27 16:08:25 by dagabrie         ###   ########.fr       */
+/*   Updated: 2023/07/27 17:01:09 by dagabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "mlx_linux/mlx.h"
+# include "../mlx_linux/mlx.h"
 # include <fcntl.h>
 # include <unistd.h>
 # include "libft/libft.h"
